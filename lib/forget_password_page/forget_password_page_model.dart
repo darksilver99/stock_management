202865bf83a45cd5df455683644410e3c8c7c6da
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-22 07:01:42 +0000

    Updating to latest FlutterFlow output.

--- lib/forget_password_page/forget_password_page_model.dart
+++ lib/forget_password_page/forget_password_page_model.dart
@@ -3,7 +3,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/custom_code/actions/index.dart' as actions;
 import 'forget_password_page_widget.dart' show ForgetPasswordPageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
