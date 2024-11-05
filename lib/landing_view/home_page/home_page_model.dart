bca87bca11314a53828d015a0904fddf64311fd3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-05 03:34:42 +0000

    Updating to latest FlutterFlow output.

--- lib/landing_view/home_page/home_page_model.dart
+++ lib/landing_view/home_page/home_page_model.dart
@@ -1,12 +1,19 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // Stores action output result for [Action Block - confirmBlock] action in Button widget.
+  bool? isConfirm;
+
   @override
   void initState(BuildContext context) {}
 
--- lib/landing_view/home_page/home_page_widget.dart
+++ lib/landing_view/home_page/home_page_widget.dart
@@ -1,6 +1,8 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -43,7 +45,46 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           top: true,
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            children: [],
+            children: [
+              FFButtonWidget(
+                onPressed: () async {
+                  Function() _navigate = () {};
+                  _model.isConfirm = await action_blocks.confirmBlock(
+                    context,
+                    title: 'ออกจากระบบ?',
+                  );
+                  if (_model.isConfirm!) {
+                    GoRouter.of(context).prepareAuthEvent();
+                    await authManager.signOut();
+                    GoRouter.of(context).clearRedirectLocation();
+
+                    _navigate =
+                        () => context.goNamedAuth('LoginPage', context.mounted);
+                  } else {
+                    safeSetState(() {});
+                  }
+
+                  _navigate();
+
+                  safeSetState(() {});
+                },
+                text: 'Button',
+                options: FFButtonOptions(
+                  height: 40.0,
+                  padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+                  iconPadding:
+                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                  color: FlutterFlowTheme.of(context).primary,
+                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                        fontFamily: 'Inter',
+                        color: Colors.white,
+                        letterSpacing: 0.0,
+                      ),
+                  elevation: 0.0,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ],
           ),
         ),
       ),
