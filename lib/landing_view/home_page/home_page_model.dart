034dac1bd5beffa7a905d7c9468c6a043846acab
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-05 09:54:30 +0000

    Updating to latest FlutterFlow output.

--- lib/landing_view/home_page/home_page_model.dart
+++ lib/landing_view/home_page/home_page_model.dart
@@ -1,24 +1,40 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/component/info_custom_view/info_custom_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/walkthroughs/add_product.dart';
 import '/actions/actions.dart' as action_blocks;
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'home_page_widget.dart' show HomePageWidget;
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart'
+    show TutorialCoachMark;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:webviewx_plus/webviewx_plus.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
   ///  Local state fields for this page.
 
   bool isLoading = true;
 
+  ///  State fields for stateful widgets in this page.
+
+  TutorialCoachMark? addProductController;
+  // Stores action output result for [Firestore Query - Query a collection] action in Container widget.
+  int? totalProduct;
+
   @override
   void initState(BuildContext context) {}
 
   @override
-  void dispose() {}
+  void dispose() {
+    addProductController?.finish();
+  }
 }
