4ea754b13e5dd832686a58e9ec8c8cf0b2b7fd46
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-07 03:27:51 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/product_view/transasction_detail_view/transasction_detail_view_model.dart
@@ -0,0 +1,21 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:math';
+import 'transasction_detail_view_widget.dart' show TransasctionDetailViewWidget;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class TransasctionDetailViewModel
+    extends FlutterFlowModel<TransasctionDetailViewWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
